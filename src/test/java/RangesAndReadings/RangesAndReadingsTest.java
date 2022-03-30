7a033a4c741d00f4252b6361c071f8020118e2e9
Swetha K A <k.a.swetha850@gmail.com>
2022-03-30 10:06:51 +0530

    Update RangesAndReadingsTest.java

--- src/test/java/RangesAndReadings/RangesAndReadingsTest.java
+++ src/test/java/RangesAndReadings/RangesAndReadingsTest.java
@@ -10,7 +10,10 @@ public class RangesAndReadingsTest {
   public void testRangesAndReadings() {
     int[] firstArray = { 4, 5, 6, 7, 10, 11, 12 };
     int[] secondArray = { 10, 13, 12, 11, 15, 14, 20 };
+    int[] a2dArray = { 1740, 2050, 2400, 2950, 3789 };
+    
     assertEquals("[4-7, 10-12]", RangesAndReadings.checkForRanges(firstArray).toString());
     assertEquals("[10-15, 20]", RangesAndReadings.checkForRanges(secondArray).toString());
+    assertEquals("[4-7, 9]", RangesAndReadings.findRangesForA2D(a2dArray).toString());
   }
 }
